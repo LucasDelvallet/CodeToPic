165d36ae308f7b79b787012033c11ec93755fbff
Quentin Delvallet <quentin.delvallet@hotmail.fr>
2017-01-27 16:44:01 +0100

    First prototype of svg gen

--- src/writer/SVGGenerator.java
+++ src/writer/SVGGenerator.java
@@ -4,13 +4,14 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
+import java.io.FileOutputStream;
 import java.io.IOException;
-
-import javax.swing.JFrame;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import org.apache.batik.anim.dom.SVGDOMImplementation;
 import org.apache.batik.svggen.SVGGraphics2D;
-import org.apache.batik.swing.JSVGCanvas;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Element;
 import org.w3c.dom.svg.SVGDocument;
@@ -19,6 +20,7 @@ import reader.ToBytes;
 
 /**
  * Work in progress. Generate a SVG from byte[].
+ * https://xmlgraphics.apache.org/batik/using/svg-generator.html
  * 
  * @author Quentin
  *
@@ -37,46 +39,35 @@ public class SVGGenerator extends Generator {
 		SVGDocument doc = createSVGDocument();
 		SVGGraphics2D g = new SVGGraphics2D(doc);
 		
+		g.setSVGCanvasSize(new Dimension(256, 256));
 		
-		/*
-		 * 
-		 * Grosse boucle qui prendrait les bytes 4 par 4.
-		 * Chaque byte peut prendre 256 valeurs du coup l'image serait de 256x256
-		 * Le premier byte donne la forme (parmi 256 formes ça sera peut-être chaud par contre)
-		 * Le second byte donne la position en x
-		 * Le troisième byte donne la position en y
-		 * Le quatrième byte donne la couleur (on programmerait 256 couleurs possibles)
-		 * 
-		 * Et s'il n'y a pas un multiple de 4, on remplit les dernières valeurs par des bytes à 0
-		 * 
-		 * Vu que c'est un svg, le process inverse devrait être possible.
-		 * 
-		 */
+		int rest = data.length % 8;
 		
-		//Tests
-		Shape circle = new Ellipse2D.Double(0, 0, 50, 50);
-	    g.setPaint(Color.red);
-	    g.fill(circle);
-	    g.translate(60, 0);
-	    g.setPaint(Color.green);
-	    g.fill(circle);
-	    g.translate(60, 0);
-	    g.setPaint(Color.blue);
-	    g.fill(circle);
-	    g.setSVGCanvasSize(new Dimension(256, 256));
-	    
-	    // Populate the document root with the generated SVG content.
-	    Element root = doc.getDocumentElement();
-	    g.getRoot(root);
+		for(int i=0; i<data.length-rest; i+=8) {
+			int data1 = data[i]+128;
+			int data2 = data[i+1]+128;
+			int data3 = data[i+2]+128;
+			int data4 = data[i+3]+128;
+			int data5 = data[i+4]+128;
+			int data6 = data[i+5]+128;
+			int data7 = data[i+6]+128;
+			int data8 = data[i+7]+128;
+			
+			Shape circle = new Ellipse2D.Double(data1, data2, data3, data4);
+			Color c = new Color(data5, data6, data7, data8);
 
-	    // Display the document.
-	    JSVGCanvas canvas = new JSVGCanvas();
-	    JFrame f = new JFrame();
-	    f.getContentPane().add(canvas);
-	    canvas.setSVGDocument(doc);
-	    f.pack();
-	    f.setVisible(true);
+			g.setPaint(c);
+			g.fill(circle);
+		}
 		
+	    // Save SVG as file
+	    OutputStream outputStream = new FileOutputStream(imagePath + "file.svg");
+		Writer out = new OutputStreamWriter(outputStream, "UTF-8");
+		g.stream(out, true);
+		outputStream.flush();
+		outputStream.close();
+		
+		System.out.println("Done !");
 	}
 	
 	private SVGDocument createSVGDocument() {
