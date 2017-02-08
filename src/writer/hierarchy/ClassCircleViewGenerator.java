a205cfdfd20be8679e57c2561b2edab85334c91d
Mickael Alvarez <alvarez.mickael.93@gmail.com>
2017-02-08 21:28:46 +0100

    Class representation is now the average of its methods representations

--- src/writer/hierarchy/ClassCircleViewGenerator.java
+++ src/writer/hierarchy/ClassCircleViewGenerator.java
@@ -1,9 +1,5 @@
 package writer.hierarchy;
 
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 import reader.ToBytes;
 import util.RGB;
 import writer.hierarchy.visitors.metrics.ClassMetric;
@@ -18,38 +14,36 @@ public class ClassCircleViewGenerator extends CircleViewGenerator implements IHi
 
 	@Override
 	public RGB getStrokeColor() {
-		MessageDigest digest;
-		int red = 0, green = 0, blue = 0;
-		try {
-			digest = MessageDigest.getInstance("SHA-256");
-			
-			if(metric.getName() == null) {
-				return new RGB(0,0,0);
-			}
-			
-			byte[] hash = digest.digest(metric.getName().getBytes(StandardCharsets.UTF_8));
-			red = hash[0];
-			green = hash[15];
-			blue = hash[31];
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
+		int red = 0, green = 0, blue = 0, childrenSize = children.size();
+
+		for (IHierarchyView child : children) {
+			RGB color = child.getStrokeColor();
+			red += color.getRed();
+			green += color.getGreen();
+			blue += color.getBlue();
 		}
-		
-		return new RGB(red, green, blue);
+
+		return new RGB((int) red / childrenSize, (int) green / childrenSize, (int) blue / childrenSize);
 	}
 
 	@Override
 	public RGB getBackgroundColor() {
-		int red, blue;
-		red = (int)((metric.getNumberOfVariableDeclaration() / (float)metric.getNumberOfLine()) * 255);
-		blue = (int)((metric.getNumberOfMethode() / (float) metric.getNumberOfLine()) * 255);
-		return new RGB(red, 255, blue);
+		int red = 0, green = 0, blue = 0, childrenSize = children.size();
+
+		for (IHierarchyView child : children) {
+			RGB color = child.getBackgroundColor();
+			red += color.getRed();
+			green += color.getGreen();
+			blue += color.getBlue();
+		}
+
+		return new RGB((int) red / childrenSize, (int) green / childrenSize, (int) blue / childrenSize);
 	}
 
 	@Override
 	public int getNumberOfLines() {
 		int childrenSize = 0;
-		for(IHierarchyView child : children) {
+		for (IHierarchyView child : children) {
 			childrenSize += child.getNumberOfLines();
 		}
 		return metric.getNumberOfLine() - childrenSize;
